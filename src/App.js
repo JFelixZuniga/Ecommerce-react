2fc8e41b66e3d3169123b1fc65510b5c2a7ea9fb
JFelixZuniga <johnfzuniga@gmail.com>
2021-03-29 20:37:15 -0300

    Agregando páginas de checkout y total

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Product from "./components/Product";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
+import CheckoutPage from "./components/CheckoutPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Products />
+      <CheckoutPage />
+      {/* <Products /> */}
       {/* <Product /> */}
     </div>
   );
